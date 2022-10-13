044b508848302faaee61269bc3d72cad858af482
Mario Robles Garcia <113978341+Marius3491@users.noreply.github.com>
2022-10-13 16:50:26 +0200

    Codigo corregido

--- S05Ejercicio28/src/Main.java
+++ S05Ejercicio28/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+   int num_int;
+   Scanner sc = new Scanner(System.in);
+    
+    do {
+      System.out.print("Por favor, introduzca un número entero: ");
+      num_int = sc.nextInt();
+      
+      if (num_int < 0) {
+        System.out.println("El número introducido no es correcto.");
+      }
+    } while (num_int < 0);
+
+    int factorial = num_int;
+    
+    if (num_int == 0) {
+      System.out.println("0! = 1");
+    } else {
+      for (int i = 1; i < num_int; i++) {
+        factorial *= i;
+      }
+
+      System.out.println(num_int + "! = " + factorial);
+    }
     }
 }
